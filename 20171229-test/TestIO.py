7bc8c156f8f5d5eb432a61c4ba664aa50c3fd796
zj <zj@webturing.com>
2017-12-30 07:08:55 +0800

    增加直方图显示功能

--- 20171229-test/TestIO.py
+++ 20171229-test/TestIO.py
@@ -1,11 +1,16 @@
 import string
-dic=dict()
-for line in open('Test.py'):
-    print(line,end='')
+
+dic = dict()
+for line in open('TestIO.py'):
     for char in line:
-        if  char in string.ascii_letters:
+        if char in string.ascii_letters:
             if char in dic:
-                dic[char]+=1
+                dic[char] += 1
             else:
-                dic[char]=1
-print(dic)
+                dic[char] = 1
+print(dic)  # DEBUG
+
+for k in string.ascii_lowercase:
+    if k not in dic:
+        dic[k] = 0
+    print(k, '#' * dic[k])
