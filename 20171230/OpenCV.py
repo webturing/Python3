318be843178af50fa312e0f8dd23d719e7a48e43
zj <zj@webturing.com>
2017-12-30 09:12:40 +0800

    OpenCV tutorial

--- 20171230/OpenCV.py
+++ 20171230/OpenCV.py
@@ -1,9 +1,33 @@
+import numpy as np
 import cv2
 
-face_patterns = cv2.CascadeClassifier(
-    '/usr/local/opt/opencv3/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
-sample_image = cv2.imread('/Users/abel/201612.jpg')
-faces = face_patterns.detectMultiScale(sample_image, scaleFactor=1.1, minNeighbors=5, minSize=(100, 100))
-for (x, y, w, h) in faces:
-    cv2.rectangle(sample_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    cv2.imwrite('/Users/abel/201612_detected.png', sample_image);
+# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+
+# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
