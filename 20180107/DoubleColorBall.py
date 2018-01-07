b38bfa8e15356c83d8db5577951bf141f41d526c
zj <zj@webturing.com>
2018-01-07 15:43:12 +0800

    标准双色球模拟程序(优化版) 本期特等奖号码: [23, 22, 15, 26, 7, 5] ( 14 ) 获奖次数:  [0, 0, 0, 0, 20, 31414] 投入金额:  1000000 获得奖金:  157270 中奖率: 6.29% 返奖率: 15.73%

--- 20180107/DoubleColorBall.py
+++ 20180107/DoubleColorBall.py
@@ -14,32 +14,38 @@ def getPrize(key, user):
             red += 1
     if key[-1] == user[-1]:
         blue += 1
+    result = 0
     if red == 6 and blue == 1:  # 6+1
-        return 1
-    if red == 6 and blue == 0:  # 6+0
-        return 2
-    if red == 5 and blue == 1:
-        return 3
-    if red + blue == 5:
-        return 4
-    if red + blue == 4:
-        return 5
-    if blue == 1:
-        return 6
-    return 0
+        result = 1
+    elif red == 6 and blue == 0:  # 6+0
+        result = 2
+    elif red == 5 and blue == 1:
+        result = 3
+    elif red + blue == 5:
+        result = 4
+    elif red + blue == 4:
+        result = 5
+    elif blue == 1:
+        result = 6
+    return (red, blue, result)
 
 
 KEY = randomSelect(1, 33, 6) + randomSelect(1, 16, 1)
-print("本期特等奖号码:", KEY[0:6], "(", KEY[-1], ")")
+print(
+    "****本期特等奖号码:%02d %02d %02d %02d %02d %02d (%02d)*****" % (KEY[0], KEY[1], KEY[2], KEY[3], KEY[4], KEY[5], KEY[6]))
 
 prizes = [0] * 7
-TOTAL = 500000
+TOTAL = 5000000
 PRIZES = [0, 5000000, 100000, 3000, 200, 10, 5, ]
 for i in range(TOTAL):
     redBalls = randomSelect(1, 33, 6)
     blueBall = randomSelect(1, 16, 1)
     user = redBalls + blueBall
-    prizes[getPrize(KEY, user)] += 1
+    red, blue, pr = getPrize(KEY, user)
+    prizes[pr] += 1
+    if pr in [1, 2, 3, 4]:
+        print("User%010d: %d PRIZE (%d+%d)" % (i, pr, red, blue),
+              "%02d %02d %02d %02d %02d %02d (%02d)" % (user[0], user[1], user[2], user[3], user[4], user[5], user[6]))
 getMoney = sum([prizes[i] * PRIZES[i] for i in range(7)])
 print("获奖次数: ", prizes[1:])
 print("投入金额: ", TOTAL * 2)
