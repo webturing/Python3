b7b30885a864c74e818d7c06106370b323f19a1a
zj <zj@webturing.com>
2018-10-24 23:02:22 +0800

    构造模拟，或者部分和

--- oj.ahstu.cc/1025.py
+++ oj.ahstu.cc/1025.py
@@ -1,6 +1,4 @@
 import math
 
 n = int(input())
-print(list(map(lambda x: 1 /x**2, range(1, n + 1))))
-s = sum(map(lambda x: 1 /x**2, range(1, n + 1)))
-print("%.6f" % (math.sqrt(6 * s)))
+print('%.6f' % (float(str(math.pi)[0:n + 3])))
