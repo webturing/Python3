18d037b42ced4ecc4bcd2557fa5fb5b107099f25
zj <zj@webturing.com>
2018-10-24 21:36:46 +0800

    filter application

--- oj.ahstu.cc/1013.py
+++ oj.ahstu.cc/1013.py
@@ -1,1 +1,5 @@
-print('(220,284)(1184,1210)(2620,2924)')
+f = lambda n: sum(list(filter(lambda x: n % x == 0, range(1, n))))
+for a in range(1, 3001):
+    b = f(a)
+    if b > a == f(b):
+        print('(%d,%d)' % (a, b), end='')
